c35e568e2de75d3f86b604688367afe829be9897
Olha Melska <olya.melska@gmail.com>
2018-08-23 22:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerRight(){
   var right = parseInt(rightNumbers, 10)
  
   if (right < 360) {
-    element.style.left = `${right + 1}px`
+    element.style.left = `${right + 10}px`
   }
 }
 
