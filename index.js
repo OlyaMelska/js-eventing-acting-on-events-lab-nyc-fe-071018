48a347a3abe381c67eab6f2ce6e8a4a7fd518392
Olha Melska <olya.melska@gmail.com>
2018-08-23 22:54:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let element = document.getElementById('dodger')
 
 function moveDodgerLeft() {
-  var leftNumbers = element.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
+  let leftNumbers = element.style.left.replace('px', '')
+  let left = parseInt(leftNumbers, 10)
  
   if (left > 0) {
     element.style.left = `${left - 1}px`
@@ -10,8 +10,8 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerUp() {
-  var topNumbers = element.style.top.replace('px', '')
-  var top = parseInt(topNumbers, 10)
+  let topNumbers = element.style.top.replace('px', '')
+  let top = parseInt(topNumbers, 10)
  
   //if (top > 0) {
     element.style.top = `${top - 10}px`
@@ -19,8 +19,8 @@ function moveDodgerUp() {
 }
 
 function moveDodgerRight(){
-  var rightNumbers = element.style.left.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
+  let rightNumbers = element.style.left.replace('px', '')
+  let right = parseInt(rightNumbers, 10)
  
   if (right < 360) {
     element.style.left = `${right + 1}px`
